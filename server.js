b66b73c472aaa9e1b796dce23f679647467fd0eb
Tshiamo <tmatiza19@gmail.com>
2024-11-04 10:02:37 +0200

    server file created

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=> console.log(`Server has started on port ${PORT}`))
+
+
+
+
+
