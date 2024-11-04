80c9c2a19341bedf03839fff308b30a4362054f8
Tshiamo <tmatiza19@gmail.com>
2024-11-04 11:38:54 +0200

    updated the firebase private key as a single json

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 
 const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes')
+
+dotenv.config()
 
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.use('api/auth',authRoutes)
+
 
 app.listen(PORT,()=> console.log(`Server has started on port ${PORT}`))
 
