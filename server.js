6e74dde33e832de22e03f358444417cb8597036c
Tshiamo <tmatiza19@gmail.com>
2024-11-05 14:50:52 +0200

    added the env file to gitignore

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 
 app.use('/api/auth', authRoutes);
